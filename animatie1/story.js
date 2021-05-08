9855ed8f0da23ce7b54363ef1cb11de53522474b
SilviuM84 <miricasilviuconstantin@yahoo.com>
2021-05-08 12:20:13 +0300

    again

---
+++ animatie1/story.js
@@ -0,0 +1,9 @@
+var hero = docucument.querySelector("#hero");
+var villain = docucument.querySelector("#villain");
+var lightning = document.querySelector("#lightning");
+
+var lightningStart = { "left": "290px" };
+var lightningEnd = { "left": "900px" };
+var options = { "duration": 1000 };
+
+lightning.animate([lightningStart, lightningEnd], options);
